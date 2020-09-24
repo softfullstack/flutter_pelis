8d4d016e0ff4a7f23b5fceee5c611e459fa6c1c1
mario_meta <desarrollo.software@metapack.com.mx>
2020-09-24 17:03:14 -0500

    swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -7,9 +8,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('home'),
+        title: Text('Peliculas en cine 📹'),
         actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swipeTarget()],
+        ),
       ),
     );
   }
+
+  _swipeTarget() => Container(
+        width: double.infinity,
+        height: 300.0,
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.network(
+              "http://via.placeholder.com/350x150",
+              fit: BoxFit.fill,
+            );
+          },
+          itemCount: 4,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
+        ),
+      );
 }
